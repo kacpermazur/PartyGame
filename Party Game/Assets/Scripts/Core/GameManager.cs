4ad7e4d2b401e5b5a5df795e437f5cbd4e79fd41
Kacper Mazur <kacper.mazur888@gmail.com>
2019-12-02 18:58:02 +0000

    Game Manager Added

--- Party Game/Assets/Scripts/Core/GameManager.cs
+++ Party Game/Assets/Scripts/Core/GameManager.cs
@@ -1,18 +1,187 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using Player;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int numberOfRoundsToWin = 3;
+    public float startCountDown = 3.0f;
+    public float endCountDown = 3.0f;
+
+    public TextMeshProUGUI gameMessage;
+    
+    public GameObject playerPrefab;
+    public PlayerManager[] players;
+
+    private int _currentRound;
+    private WaitForSeconds _startWait;
+    private WaitForSeconds _endWait;
+
+    private PlayerManager _roundWinner;
+    private PlayerManager _gameWinner;
+    
+    private void Start()
     {
+        _startWait = new WaitForSeconds(startCountDown);
+        _endWait = new WaitForSeconds(endCountDown);
         
+        InitializeAllPlayer();
+
+        StartCoroutine(Game());
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeAllPlayer()
     {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].playerInstance =
+                Instantiate(playerPrefab, players[i].SpawnPoint.position, players[i].SpawnPoint.rotation);
+            players[i].playerID = i + 1;
+            players[i].Initialize();
+        }
+    }
+
+    private void ResetPlayers()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].Reset();
+        }
+    }
+    private void EnablePlayersControls()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].EnableControls();
+        }
+    }
+
+    private void DisablePlayersControls()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].DisableControls();
+        }
+    }
+
+    private bool LastPlayerCheck()
+    {
+        int numberOfPlayersLeft = 0;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].playerInstance.activeSelf)
+            {
+                numberOfPlayersLeft++;
+            }
+        }
+
+        return numberOfPlayersLeft <= 1;
+    }
+
+    private PlayerManager RoundWinner()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].playerInstance.activeSelf)
+                return players[i];
+        }
+
+        return null;
+    }
+    
+    private PlayerManager GameWinner()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].numberOfWins == numberOfRoundsToWin)
+                return players[i];
+        }
+
+        return null;
+    }
+
+    private String RoundEndMessage()
+    {
+        string message = "Draw";
+
+        if (_roundWinner != null)
+            message = _roundWinner.playerName + "Wins The Round!";
+        message += "\n\n\n\n";
         
+        message += "SCORES: \n";
+        
+        for (int i = 0; i < players.Length; i++)
+        {
+            message += players[i].playerName + ": " + players[i].numberOfWins + " Wins/n";
+        }
+
+        if (_gameWinner != null)
+        {
+            message = _gameWinner.playerName + " WINS THE GAME!";
+        }
+        
+        return message;
+    }
+
+    private IEnumerator Game()
+    {
+        yield return StartCoroutine(StartRound());
+        yield return StartCoroutine(InRound());
+        yield return StartCoroutine(EndRound());
+
+        if (_gameWinner != null)
+        {
+            SceneManager.LoadScene(0); // ToDO: Add Mini Game Change
+        }
+        else
+        {
+            StartCoroutine(Game());
+        }
+    }
+
+    private IEnumerator StartRound()
+    {
+        ResetPlayers();
+        DisablePlayersControls();
+        _currentRound++;
+
+        gameMessage.text = "Round: " + _currentRound;
+        yield return _startWait;
+    }
+
+    private IEnumerator InRound()
+    {
+        EnablePlayersControls();
+        gameMessage.text = String.Empty;
+
+        while (!LastPlayerCheck())
+        {
+            yield return null;
+        }
+    }
+
+    private IEnumerator EndRound()
+    {
+        DisablePlayersControls();
+
+        _roundWinner = null;
+        _roundWinner = RoundWinner();
+
+        if (_roundWinner != null)
+            _roundWinner.numberOfWins++;
+
+        _gameWinner = null;
+        _gameWinner = GameWinner();
+        
+        if (_roundWinner != null)
+            _roundWinner.numberOfWins++;
+        
+        gameMessage.text = RoundEndMessage();
+
+        yield return endCountDown;
     }
 }
--- Party Game/Assets/Scripts/Player/PlayerManager.cs
+++ Party Game/Assets/Scripts/Player/PlayerManager.cs
@@ -2,63 +2,64 @@
 using Player;
 using UnityEngine;
 
-[Serializable]
-public class PlayerManager
+namespace Player
 {
-    public Color PlayerColor;
-    public Transform SpawnPoint;
-
-    public int playerID;
-    public string playerName;
-    public GameObject playerInstance;
-    public int numberOfWins;
-
-    private PlayerMovement _movement;
-    private PlayerAttack _attack;
-    private CollisionListener _listener;
-
-    void Initialize()
+    [Serializable]
+    public class PlayerManager
     {
-        _movement = playerInstance.GetComponent<PlayerMovement>();
-        _attack = playerInstance.GetComponent<PlayerAttack>();
-        _listener = playerInstance.GetComponent<CollisionListener>();
+        public Color PlayerColor;
+        public Transform SpawnPoint;
 
-        _movement.playerID = playerID;
-        _attack.playerID = playerID;
-        _listener.playerID = playerID;
-        
-        playerName = "<color=#" + ColorUtility.ToHtmlStringRGB(PlayerColor) + ">PLAYER " + playerName + "</color>";
+        [HideInInspector] public int playerID;
+        [HideInInspector] public string playerName;
+        [HideInInspector] public GameObject playerInstance;
+        [HideInInspector] public int numberOfWins;
 
-        MeshRenderer[] renderers = playerInstance.GetComponentsInChildren<MeshRenderer>();
+        private PlayerMovement _movement;
+        private PlayerAttack _attack;
+        private CollisionListener _listener;
 
-        for (int i = 0; i < renderers.Length; i++)
+        public void Initialize()
         {
-            renderers[i].material.color = PlayerColor;
+            _movement = playerInstance.GetComponent<PlayerMovement>();
+            _attack = playerInstance.GetComponent<PlayerAttack>();
+            _listener = playerInstance.GetComponentInChildren<CollisionListener>();
+
+            _movement.playerID = playerID;
+            _attack.playerID = playerID;
+            _listener.playerID = playerID;
+
+            playerName = "<color=#" + ColorUtility.ToHtmlStringRGB(PlayerColor) + ">PLAYER " + playerName + "</color>";
+
+            MeshRenderer[] renderers = playerInstance.GetComponentsInChildren<MeshRenderer>();
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                renderers[i].material.color = PlayerColor;
+            }
+        }
+
+        public void EnableControls()
+        {
+            _movement.enabled = true;
+            _attack.enabled = true;
+            _listener.enabled = true;
+        }
+
+        public void DisableControls()
+        {
+            _movement.enabled = false;
+            _attack.enabled = false;
+            _listener.enabled = false;
+        }
+
+        public void Reset()
+        {
+            playerInstance.transform.position = SpawnPoint.position;
+            playerInstance.transform.rotation = SpawnPoint.rotation;
+
+            playerInstance.SetActive(false);
+            playerInstance.SetActive(true);
         }
     }
-
-    public void EnableControls()
-    {
-        _movement.enabled = true;
-        _attack.enabled = true;
-        _listener.enabled = true;
-    }
-    
-    public void DisableControls()
-    {
-        _movement.enabled = false;
-        _attack.enabled = false;
-        _listener.enabled = false;
-    }
-
-    public void Reset()
-    {
-        playerInstance.transform.position = SpawnPoint.position;
-        playerInstance.transform.rotation = SpawnPoint.rotation;
-        
-        playerInstance.SetActive(false);
-        playerInstance.SetActive(true);
-    }
 }
-
-
