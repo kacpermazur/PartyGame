591a98e33f0988f23f5dafe1f3cbba432b554244
Kacper Mazur <kacper.mazur888@gmail.com>
2020-01-07 04:50:17 +0000

    UI manager

---
+++ Party Game/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.Panel
+{
+    public class UIManager : MonoBehaviour, IInitializable
+    {
+        [SerializeField] private UIMainMenu _mainMenu;
+
+        public UIMainMenu MainMenuUI => _mainMenu;
+        
+        private List<UIPanel> _uiPanels = new List<UIPanel>();
+        
+        public void Initialize()
+        {
+            _uiPanels.Add(_mainMenu);
+            
+            _mainMenu.Initialize();
+        }
+        
+        private void OnDestroy()
+        {
+            RemoveListeners();
+        }
+
+        private void AddListeners()
+        {
+            _mainMenu.onButtonStartClicked += OnBtnStartClicked;
+        }
+        
+        private void RemoveListeners()
+        {
+            _mainMenu.onButtonStartClicked -= OnBtnStartClicked;
+        }
+        
+        public void OpenPanel(UIPanel uiPanel)
+        {
+            foreach (UIPanel panel in _uiPanels)
+            {
+                panel.Close();
+            }
+
+            uiPanel.Open();
+        }
+
+        private void OnBtnStartClicked()
+        {
+            
+        }
+    }
+}
